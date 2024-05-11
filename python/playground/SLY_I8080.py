129359b4f387cf0c83c974686165721d6d21037f
Robert Valentine <paraboliclabs@gmail.com>
2024-05-11 15:14:01 -0400

    add reference and playground :dragon_face: :dragon: sly update :dragon_face: :dragon:
    :dragon_face: :dragon: sly 8080 parser is very broken. :dragon: :dragon_face:

--- python/playground/SLY_I8080.py
+++ python/playground/SLY_I8080.py
@@ -2,22 +2,55 @@ from sly import Lexer, Parser
 
 
 class I8080Lexer(Lexer):
+
     tokens = {
-        LABEL, INSTRUCTION, REGISTER, IMMEDIATE_DATA, MEMORY_ADDRESS, COMMA
+        LABEL, INSTRUCTION,
+        COMMA, SPECIAL_CHARACTER,
+        IMMEDIATE_DATA, MEMORY_ADDRESS, DECIMAL,
+        SINGLE_QUOTED_CHAR, MATH_EXPRESSION,
     }
 
-    ignore = ' \t'
+    ignore = ' \t\''
     ignore_comment = r';.*'
 
-    LABEL = r'\w+:'
-      
-    INSTRUCTION = \
-        r'(MOV|ADD|SUB|INR|DCR|CMA|CMP|ANA|XRA|ORA|ADI|ACI|SUI|SBI|ANI|XRI|ORI|CALL|RET|JMP|JC|JNC|JZ|JNZ|JP|JM|JPE|JPO|HLT|PCHL|SPHL|XCHG|XTHL|DI|EI|NOP|RLC|RRC|RAL|RAR|STC|CMC|HLT)'
+    #  LABEL = r'\w+:'
+    #  LABEL = r'\b\w+:'
 
-    REGISTER = r'[A-HL]|M'
-    IMMEDIATE_DATA = r'(0[xX][0-9A-Fa-f]+|\d+)[Hh]?'
-    MEMORY_ADDRESS = r'\w+'
+    @_(r'\b\w+:')
+    def LABEL(self, t):
+        t.value = t.value.strip(':')
+        return t
+
+
+    INSTRUCTION = r'(EQU|ORG|MOV|ADD|SUB|INR|DCR|CMA|CMP|ANA|XRA|ORA|ADI|ACI|SUI|SBI|ANI|XRI|ORI|CALL|RET|JMP|JC|JNC|JZ|JNZ|JP|JM|JPE|JPO|HLT|PCHL|SPHL|XCHG|XTHL|DI|EI|NOP|RLC|RRC|RAL|RAR|STC|CMC|HLT|STAX|INX|MVI|PUSH|POP|RNZ|RP|RZ|CM|DAD|RC|CPI|LXI|RNC|CNZ|LHLD|DB|DCX|LDAX|DW)'
+
     COMMA = r','
+
+    MATH_EXPRESSION = r'[+\-]'
+    #  MATH_EXPRESSION = r'[A-Za-z0-9_]+(?:[+\-][A-Za-z0-9_]+)*'
+
+    SINGLE_QUOTED_CHAR = r'\'([^\']*)\''
+
+
+    IMMEDIATE_DATA = r'(-)?\b(?:0x[0-9A-Fa-f]+|\d+(?:\.\d+)?(?:[Hh])?)\b'
+    #  IMMEDIATE_DATA = r'(-)?\b(?:0x[0-9A-Fa-f]+|\d+(?:\.\d+)?(?:[Hh])?)\b(?![\+\-\*\/\.\'])'
+    #  IMMEDIATE_DATA = r'(-)?\b(?:0x[0-9A-Fa-f]+|\d+(?:\.\d+)?(?:[Hh])?)(?![\+\-\*\/\.])'
+    #  IMMEDIATE_DATA = r'(-)?\b(?:0x[0-9A-Fa-f]+|\d+(?:\.\d+)?(?:[Hh])?)\b(?![\+\-\*\/\.])'
+    #  IMMEDIATE_DATA = r'(-)?\b(?:0x[0-9A-Fa-f]+|\d+(?:\.\d+)?(?:[Hh])?)\b(?![\+\-\*\/])'
+    #  IMMEDIATE_DATA = r'(-)?\b(?:0x[0-9A-Fa-f]+|\d+(?:\.\d+)?(?:[Hh])?)(?!\w)'
+    #  IMMEDIATE_DATA = r'-?\b(?:0x[0-9A-Fa-f]+|\d+(?:\.\d+)?(?:[Hh])?)\b'
+    #  IMMEDIATE_DATA = r'(-)?\b(?:0x[0-9A-Fa-f]+|[0-9]+(?:\.[0-9]+)?)(?:[Hh]|\b)'
+    #  IMMEDIATE_DATA = r'(-)?\b(?:0x[0-9A-Fa-f]+|[0-9]+(?:\.[0-9]+)?)(?:[Hh])?\b'
+    #  IMMEDIATE_DATA = r'(-)?([0-9]+)(.)?(?:0)?([Xx])?([0-9A-Fa-f]+)?([Hh])?'
+    #  IMMEDIATE_DATA = r'(?:0[Xx])?[0-9A-Fa-f]+[Hh]'
+
+    #  MEMORY_ADDRESS = r'\b(?![0-9])[A-Za-z_][A-Za-z0-9_]*(?!\w|[\+\-\*\/])\b'
+    MEMORY_ADDRESS = r'\b(?![0-9])[A-Za-z_][A-Za-z0-9_]*(?!\w|[\+\-\*\/])'
+
+    SPECIAL_CHARACTER = r'[\!\@\#\$\%\^\&\*\(\)\=\`\~\{\}\[\]\:\;\"\,\/\<\>\?\\\|]'
+    #  SPECIAL_CHARACTER = r'\'[\!\@\#\$\%\^\&\*\(\)\_\=\.\`\~\{\}\[\]\:\;\"\,\/\<\>\?\\\|]\''
+
+    DECIMAL = r'(-)?(?:\d+\.\d+)'
 
     '''
     multiple regexes and data conversion
@@ -46,14 +79,14 @@ class I8080Parser(Parser):
     def __init__(self):
         self.ast = []
 
+    @_('')
+    def program(self, p):
+        return []
+
     @_('program statement')
     def program(self, p):
         p.program.append(p.statement)
         return p.program
-
-    @_('')
-    def program(self, p):
-        return []
 
     @_('statement')
     def program(self, p):
@@ -61,35 +94,64 @@ class I8080Parser(Parser):
 
     @_('LABEL statement')
     def statement(self, p):
-        return ('LABEL', p.LABEL, p.statement)
+        return [p.LABEL] + p.statement
+
+    @_('INSTRUCTION')
+    def statement(self, p):
+        return [p[0]]
 
     @_('INSTRUCTION operands')
     def statement(self, p):
-        return ('INSTRUCTION', p.INSTRUCTION, p.operands)
-
-    @_('operand COMMA operand')
-    def operands(self, p):
-        return (p.operand0, p.operand1)
+        return [p.INSTRUCTION] + p.operands
 
     @_('operand')
     def operands(self, p):
-        return (p.operand)
+        return [p[0]]
 
-    @_('REGISTER')
-    def operand(self, p):
-        return p[0]
+    @_('operands COMMA operand')
+    def operands(self, p):
+        return [p.operands, p.COMMA, p.operand]
+
+    # @_('operands MATH operand')
+    # def operands(self, p):
+    #     return [p.operands, p.MATH, p.operand]
+
+    @_('MEMORY_ADDRESS COMMA operands')
+    def operands(self, p):
+        return [p.MEMORY_ADDRESS] + p.operands
+
+    @_('DECIMAL COMMA operands')
+    def operands(self, p):
+        return [p.DECIMAL] + p.operands
 
     @_('IMMEDIATE_DATA')
     def operand(self, p):
-        return p[0]
+        return [p.IMMEDIATE_DATA]
 
     @_('MEMORY_ADDRESS')
     def operand(self, p):
-        return p[0]
+        return [p.MEMORY_ADDRESS]
+
+    @_('SPECIAL_CHARACTER')
+    def operand(self, p):
+        return [p.SPECIAL_CHARACTER]
+
+    @_('DECIMAL')
+    def operand(self, p):
+        return [p.DECIMAL]
+
+    @_('MATH_EXPRESSION')
+    def operand(self, p):
+        return [p.MATH_EXPRESSION]
+
+    @_('SINGLE_QUOTED_CHAR')
+    def operand(self, p):
+        return [p.SINGLE_QUOTED_CHAR]
 
     def error(self, p):
-        print(f"Syntax error at line {p.lineno}")
-        print(f"Syntax error: {p}")
+        print(f"Syntax error at line: {p.lineno}")
+        print(f"\t{p}")
+        print()
 
 
 def test_sly_lexer(code):
@@ -127,7 +189,30 @@ input_code = \
         ADD A, 2050H
         SUB B, 0x001A
         MOV M, A
+        
+        LABEL4:     MOV 0FH, 0x00CH
+        DB 0,78H,80H,84H,88H,8AH,8CH,8EH,90H,91H,92H
+        DB 3C00H,   4170H, 4248H,       4900H
+        DW F16_1, F16_10,  5640H,   63D0H,    70D2H
         """
 
 test_sly_lexer(input_code)
 test_sly_parser(input_code)
+
+
+def test_fp16():
+    #  test fp16.ASM
+    file_path = "example_asm/8080_fp16.ASM"
+    print(f"\nTesting with {file_path} :")
+    fp16_file = """"""
+    try:
+        with open(file_path, 'r') as asm_file:
+            fp16_file = asm_file.read()
+            test_sly_lexer(fp16_file)
+            test_sly_parser(fp16_file)
+
+    except Exception as e:
+        print(e)
+
+
+test_fp16()
